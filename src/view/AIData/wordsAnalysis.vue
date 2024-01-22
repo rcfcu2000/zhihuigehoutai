3a5c4d916aa3a743eb5b3d98371e8b3ccacad6c8
dtl <darksunnydong@qq.com>
2024-01-22 18:22:14 +0800

    修复css样式

--- src/view/AIData/wordsAnalysis.vue
+++ src/view/AIData/wordsAnalysis.vue
@@ -3,7 +3,7 @@
  * @Author: dtl darksunnydong@qq.com
  * @Date: 2024-01-22 14:35:35
  * @LastEditors: dtl darksunnydong@qq.com
- * @LastEditTime: 2024-01-22 18:17:26
+ * @LastEditTime: 2024-01-22 18:21:55
  * @FilePath: \zhihuigehoutai\src\view\AIData\wordsAnalysis.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -50,6 +50,6 @@ console.log(userStore.userInfo.nickName, "userinfoID")
     color: rgba(255, 255, 255, 1);
 }
 .page_words{
-    padding: ;
+    padding: 0 0 0 5vm;
 }
 </style>
